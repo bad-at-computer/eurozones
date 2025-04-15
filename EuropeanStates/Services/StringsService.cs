e938b3d0571353fea56ac941bc1314562f2d1a2a
johnbenjamin <jbenjamin@stonewallsolutions.com>
2025-04-15 13:23:12 +0200

    Made lots of minor modifications, components, styling

---
+++ EuropeanStates/Services/StringsService.cs
@@ -0,0 +1,30 @@
+namespace EuropeanStates.Services;
+
+public class StringsService
+{
+
+    public string TitleEurozones => "Eurozones | A visual guide to Europe's money, borders, and power structures";
+
+    public string AppDescription =>
+        "Select one or more European organizations using the checkboxes below, and view the results in the map and table (scroll down to see).";
+    public string LabelEU => "European Union (EU)";
+    public string LabelEEA => "European Economic Area (EEA)";
+    public string LabelEurozone => "Eurozone";
+    public string LabelSchengen => "Schengen Area";
+    public string LabelNATO => "NATO";
+    
+    public string DescriptionEU =>
+        "A political and economic union of European countries that collaborate on policies, trade, and governance, with shared institutions and laws.";
+
+    public string DescriptionEEA =>
+        "An agreement that extends the EU’s single market to include non-EU countries, allowing free movement of goods, services, capital, and people.";
+
+    public string DescriptionEurozone =>
+        "A group of EU countries that have adopted the Euro as their official currency and coordinate monetary policy through the European Central Bank.";
+
+    public string DescriptionSchengen =>
+        "A zone of European countries that have abolished internal border controls, allowing free and unrestricted movement of people across member states.";
+
+    public string DescriptionNATO =>
+        "North Atlantic Treaty Organization: A military alliance of countries from Europe and North America that provides collective defense and security cooperation against threats.";
+}
